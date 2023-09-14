5dd827d3211777f7b5ffd1290023d622cebb5ff6
Roman Reings <Rajkumar>
2023-09-14 19:23:17 +0530

    Added a new file/modified file

---
+++ mongoDB.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+R=\e[31m
+G=\e[32m
+Y=\e[33m
+N=\e[0m
+USERID=$(id -u)
+if [ $USERID -ne 0 ]
+then 
+    echo -e "$R ERROR: You are not Root User..$N"
+    exit 1
+fi
+yum install git -y
