a0b31fa6bac0c2071d5d7d7ff86ebdb3ebe30389
Roman Reings <Rajkumar>
2023-09-16 12:21:01 +0530

    Added MongoDB

--- mongoDB.sh
+++ mongoDB.sh
@@ -3,7 +3,10 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-PACKEGE=$(yum list installed)
+DATE=$(date +%f)
+LOGDIR=/tmp
+SCRIPT_NAME=$0
+LOGFILE=$LOGDIR/$SCRIPT_NAME-$DATE.log
 USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then 
@@ -13,11 +16,27 @@ fi
 VALIDATE(){
 if [ $1 -ne 0 ]
 then  
-    echo -e "$R Installation..$2..FAILED $N"
+    echo -e "$R $2..FAILED $N"
     exit 1
 else    
-    echo -e "$G Installation..$2..SUCCESS $N"
+    echo -e "$G $2..SUCCESS $N"
 fi
 }
 
-yum install postfix -y
+cp /home/centos/Robo-Shop-Shell/mongo.repo /etc/yum.repos.d/mongo.repo
+VALIDATE $? "copying mongo.repo file"
+
+yum install mongodb-org -y
+VALIDATE $? "Installing MongoDB"
+
+systemctl enable mongod
+VALIDATE $? "enable mongod"
+
+systemctl start mongod
+VALIDATE $? "start mongod"
+
+sed i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+VALIDATE $? "chagned mongod config"
+
+systemctl restart mongod
+VALIDATE $? "restart mongod"
