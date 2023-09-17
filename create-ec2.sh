1b9b6e42e0592149ddebdcfe13ab4456d28335b1
Roman Reings <Rajkumar>
2023-09-17 15:03:37 +0530

    Added file/modified

--- create-ec2.sh
+++ create-ec2.sh
@@ -3,6 +3,7 @@ IMAGE_ID=ami-03265a0778a880afb
 INSTANCE_TYPE=t2.micro
 SECURITY_GROUP=sg-0f8c523d0f4ed489b
 HOTSTED_ZONE=Z0997824248HW2XYA9N5U
+DOMAIN_NAME=venkateshamsonnalia143.online
 NAME=("catalog" "mongodb" "user" "cart" "mysql" "rabbitmq" "redis" "web" "payment" "shipping")
 for i in "${NAME[@]}"
 do 
@@ -14,7 +15,7 @@ do
             "Changes": [{
             "Action": "CREATE",
                         "ResourceRecordSet": {
-                                "Name": "'$i.$HOSTED_ZONE'",
+                                "Name": "'$i.$DOMAIN_NAME'",
                                 "Type": "A",
                                 "TTL": 1,
                         "ResourceRecords": [{ "'Value": "$IP_ADDRESS'"}]
