eb22e09a6edddf439abaf8ad6dbd7b99e4a40306
Roman Reings <Rajkumar>
2023-09-17 14:28:29 +0530

    Added file/modified

--- create-ec2.sh
+++ create-ec2.sh
@@ -4,4 +4,5 @@ NAME=("catalog" "mongodb" "user" "cart" "mysql" "rabbitmq" "redis" "web" "paymen
 for i in "${NAME[@]}"
 do 
     echo " Name $num: $i "
+    aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type t2.micro --security-group-ids sg-0f8c523d0f4ed489b
 done
