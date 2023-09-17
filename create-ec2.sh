3ea882053bc9f58981bbb0100a895179979cc99c
Roman Reings <Rajkumar>
2023-09-17 14:57:20 +0530

    Added file/modified

--- create-ec2.sh
+++ create-ec2.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
-
+IMAGE_ID=ami-03265a0778a880afb
+INSTANCE_TYPE=t2.micro
+SECURITY_GROUP=sg-0f8c523d0f4ed489b
+HOTSTED_ZONE=Z0997824248HW2XYA9N5U
 NAME=("catalog" "mongodb" "user" "cart" "mysql" "rabbitmq" "redis" "web" "payment" "shipping")
 for i in "${NAME[@]}"
 do 
     echo " Name $num: $i "
-    aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type t2.micro --security-group-ids sg-0f8c523d0f4ed489b --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress'
+    IP_ADDRESS=(aws ec2 run-instances --image-id $IMAGE_ID --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
 
+    $ aws route53 change-resource-record-sets --hosted-zone-id Z0997824248HW2XYA9N5U --change-batch '
+    {
+            "Changes": [{
+            "Action": "CREATE",
+                        "ResourceRecordSet": {
+                                "Name": "'$i.$HOSTED_ZONE'",
+                                "Type": "A",
+                                "TTL": 1,
+                        "ResourceRecords": [{ "'Value": "$IP_ADDRESS'"}]
+    }}]
+    }'
 done
