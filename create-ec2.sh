a4622586cb1bee8b396e12e4504e95b7ae05c49c
Roman Reings <Rajkumar>
2023-09-17 14:21:40 +0530

    Added file/modified

--- create-ec2.sh
+++ create-ec2.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 NAME=("catalog" "mongodb" "user" "cart" "mysql" "rabbitmq" "redis" "web" "payment" "shipping")
-num=1
 for i in "${NAME[@]}"
 do 
     echo " Name $num: $i "
-    num=$num+1
 done
