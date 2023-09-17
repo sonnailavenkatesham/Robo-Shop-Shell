af71eefc23f96a7a0d4b4541c069f52b4c1b62fb
Roman Reings <Rajkumar>
2023-09-17 14:43:20 +0530

    Added file/modified

--- create-ec2.sh
+++ create-ec2.sh
@@ -4,6 +4,6 @@ NAME=("catalog" "mongodb" "user" "cart" "mysql" "rabbitmq" "redis" "web" "paymen
 for i in "${NAME[@]}"
 do 
     echo " Name $num: $i "
-    aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type t2.micro --security-group-ids sg-0f8c523d0f4ed489b --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=$i}]' | jq -r '.Instances[0].PrivateIpAddress'
+    aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type t2.micro --security-group-ids sg-0f8c523d0f4ed489b --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress'
 
 done
